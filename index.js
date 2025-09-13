d2977f4fd0f8ee7362a54edaaebffff81677d0bb
Grace Odongo <graceakinyi024@gmail.com>
2025-09-13 13:13:55 +0300

    updates index file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName="bob";
+function upperCaseCustomerName(){
+    return customerName.toUpperCase()
+}
+
+
+function setBestCustomer(){
+    var bestCustomer="not bob";
+  return bestCustomer
+}
+
+
+function overwriteBestCustomer(){
+  return bestCustomer='maybe bob'
+}
+const leastFavoriteCustomer="Mercy"
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer="June"
+}
+console.log(upperCaseCustomerName())
+console.log(setBestCustomer())
+console.log(overwriteBestCustomer())
+console.log(leastFavoriteCustomer())
