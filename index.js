fc2ec8b229f786aca3d837ff8015127ec0859652
Grace Odongo <graceakinyi024@gmail.com>
2025-09-25 14:20:48 +0300

    saves

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-var customerName="bob";
-function upperCaseCustomerName(){
-    return customerName.toUpperCase()
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
 }
 
-var bestCustomer="not bob";
-function setBestCustomer(){    
-  return bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
 }
 
+const leastFavoriteCustomer = 'alice';
 
-function overwriteBestCustomer(){
-  return bestCustomer='maybe bob'
+function changeLeastFavoriteCustomer() {
+
+  leastFavoriteCustomer = 'eve';
 }
-const leastFavoriteCustomer="Mercy"
-function changeLeastFavoriteCustomer(){
-    return leastFavoriteCustomer="June"
-}
-console.log(upperCaseCustomerName())
-console.log(setBestCustomer())
-console.log(overwriteBestCustomer())
-console.log(leastFavoriteCustomer())
